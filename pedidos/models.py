6ba90f5d4bbae66ca9465be62ae912528eb66da7
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-18 16:03:16 -0300

    Update models.py

--- pedidos/models.py
+++ pedidos/models.py
@@ -9,7 +9,7 @@ from produtos.models import Produto, Variacao
 
 
 class OpcaoFrete(models.Model):
-    # 🚨 FORÇAR MUDANÇA: Adicionar 'blank=True' 🚨
+    # 🚨 Adicione temporariamente 'blank=True' 🚨
     nome = models.CharField(max_length=100, unique=True, blank=True) 
     custo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     ativo = models.BooleanField(default=True)
