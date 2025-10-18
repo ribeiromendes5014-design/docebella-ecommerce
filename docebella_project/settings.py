0f05a3f1067b592d1d3a6e499a286620cb06826c
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-18 09:28:04 -0300

    Update settings.py

--- docebella_project/settings.py
+++ docebella_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # A SECRET_KEY será definida no bloco CONFIGURAÇÕES DE PRODUÇÃO abaixo.
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = true
 
 ALLOWED_HOSTS = [] # Será sobrescrito no bloco CONFIGURAÇÕES DE PRODUÇÃO
 
