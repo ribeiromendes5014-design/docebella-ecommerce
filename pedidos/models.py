51a5cbd7dc53c31d925be80779005571f06f9819
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-18 15:48:04 -0300

    Update models.py

--- pedidos/models.py
+++ pedidos/models.py
@@ -8,6 +8,14 @@ from produtos.models import Produto, Variacao
 from produtos.models import Produto, Variacao
 
 
+class OpcaoFrete(models.Model):
+    nome = models.CharField(max_length=100, unique=True) # "Retirada Local"
+    custo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) # 0.00
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+        
 class EnderecoEntrega(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255)
