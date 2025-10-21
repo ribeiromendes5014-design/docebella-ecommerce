dd8cd41d5e8b9d84c93fad197a3c61342a65569f
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-21 18:39:59 -0300

    Update admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+# 🌸 Personalização visual do painel
+admin.site.site_header = "Doce & Bella - Painel Administrativo 💅"
+admin.site.site_title = "Administração Doce & Bella"
+admin.site.index_title = "Bem-vinda ao Painel de Gestão, Bella!"
+
