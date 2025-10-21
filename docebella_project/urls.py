5609a0361673c3e02c6addfd513b5bd8eb680617
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-21 20:43:43 -0300

    Update urls.py

--- docebella_project/urls.py
+++ docebella_project/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+# path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
 
 # ==========================================
 # 🔐 PROTEÇÃO DO ADMIN (Honeypot + Caminho Secreto)
