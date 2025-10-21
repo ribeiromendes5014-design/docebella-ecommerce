4a4c6788913e75656635f998238949a55b26beef
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-20 23:59:14 -0300

    Update settings.py

--- docebella_project/settings.py
+++ docebella_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'usuarios', 
     'pedidos',  
 ]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
