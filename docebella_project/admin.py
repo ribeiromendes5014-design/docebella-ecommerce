ffe038897ec0f095cd1efae6d84c840174b036cd
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-21 23:14:06 -0300

    Create admin.py

---
+++ docebella_project/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Personalização visual do painel admin
+admin.site.site_header = "Painel Administrativo - Doce & Bella 💖"
+admin.site.site_title = "Doce & Bella | Admin"
+admin.site.index_title = "Bem-vinda ao Painel de Controle ✨"
