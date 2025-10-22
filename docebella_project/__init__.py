804a8d1bf624be68ef877ff8e6dadff756b8a8dc
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-21 23:13:37 -0300

    Update __init__.py

--- docebella_project/__init__.py
+++ docebella_project/__init__.py
@@ -1,6 +1,1 @@
-from django.contrib import admin
 
-# Personaliza o título, subtítulo e nome da aba do navegador
-admin.site.site_header = "Painel Administrativo - Doce & Bella 💖"
-admin.site.site_title = "Doce & Bella | Admin"
-admin.site.index_title = "Bem-vinda ao Painel de Controle ✨"
