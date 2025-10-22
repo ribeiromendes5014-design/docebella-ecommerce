df6028d321decc1d0584d675b499d81976bb1564
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-22 17:15:32 -0300

    Update settings.py

--- docebella_project/settings.py
+++ docebella_project/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab',
+   
 
     # Extras Django
     'django.contrib.humanize',
