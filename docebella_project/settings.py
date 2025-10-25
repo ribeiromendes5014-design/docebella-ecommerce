0454f9f38676ffe3ce3690480e3152148b795e28
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-24 21:59:52 -0300

    Update settings.py

--- docebella_project/settings.py
+++ docebella_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nested_admin',
 
     # Extras Django
     'django.contrib.humanize',
